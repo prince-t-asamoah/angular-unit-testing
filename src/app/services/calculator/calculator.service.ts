0e89ce8d32abc65965525e2e81c8cff29e98c1db
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-09 16:24:02 +0000

    use logger service in add and subtract method

--- src/app/services/calculator/calculator.service.ts
+++ src/app/services/calculator/calculator.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from '../logger/logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalculatorService {
-  constructor() {}
+  constructor(private _loggerService: LoggerService) {}
 
   add(n1: number, n2: number): number {
+    this._loggerService.log('Add operation successful');
     return n1 + n2;
   }
 
   subtract(n1: number, n2: number): number {
+    this._loggerService.log('Subtract operation successful');
     return n1 - n2;
   }
 }
