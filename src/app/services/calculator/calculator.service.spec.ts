ef36deae368cc86379ba72d41359189ab9df402d
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-09 16:01:06 +0000

    setup test cases

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,16 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-
-import { CalculatorService } from './calculator.service';
-
 describe('CalculatorService', () => {
-  let service: CalculatorService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+  //using xit and pending in test case body disable test case
+  it('should add two numbers', () => {
   });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should subtract two numbers', () => {
   });
 });
