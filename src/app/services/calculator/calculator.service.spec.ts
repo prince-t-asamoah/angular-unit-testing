2b3a2ab33822b41e32c9edc808628d0829206790
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-09 16:08:29 +0000

    implement first test case logic

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,7 +1,15 @@
+import { CalculatorService } from './calculator.service';
+
 describe('CalculatorService', () => {
   //using xit and pending in test case body disable test case
   it('should add two numbers', () => {
+    let expectedResult: number;
+    const calculator = new CalculatorService();
+
+    const result = calculator.add(2, 2);
+    expectedResult = 4;
+    
+    expect(result).toBe(expectedResult);
   });
-  it('should subtract two numbers', () => {
-  });
+  xit('should subtract two numbers', () => {});
 });
