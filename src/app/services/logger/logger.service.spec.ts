caf494030e943254674198e199f7119f9a25b849
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 08:38:53 +0000

    setup a teardown before each test case

--- src/app/services/logger/logger.service.spec.ts
+++ src/app/services/logger/logger.service.spec.ts
@@ -1,14 +1,19 @@
 import { LoggerService } from './logger.service';
 
 describe('LoggerService', () => {
+
+  let loggerService: LoggerService;
+
+  beforeEach(() => {
+    loggerService = new LoggerService();
+  });
+
   it('should start with no message', () => {
-    const loggerService = new LoggerService();
     let messagesCount = loggerService.messages.length;
     expect(messagesCount).toBe(0);
   });
 
   it('should add message when log is called', () => {
-    const loggerService = new LoggerService();
     loggerService.log('message');
     expect(loggerService.messages.length).toBe(1);
   });
