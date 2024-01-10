34782d893ba11968cf559f5cd74e3c988fc92067
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 09:14:55 +0000

    implement get all post and delete post methods

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../../model/post';
+import { PostService } from '../../services/post/post.service';
 
 @Component({
   selector: 'app-post',
@@ -11,5 +12,20 @@ import { Post } from '../../model/post';
 export class PostComponent implements OnInit {
   posts: Post[] = [];
 
-  ngOnInit(): void {}
+  constructor(private _postService: PostService) {}
+
+  ngOnInit(): void {
+    this.getAllPosts();
+  }
+
+  getAllPosts(): void {
+    this._postService.getPosts().subscribe((posts) => {
+      this.posts = posts;
+    });
+  }
+
+  deletePost(post: Post) {
+    this.posts = this.posts.filter(postItem => postItem.id !== post.id);
+    this._postService.deletePost(post).subscribe();
+  }
 }
