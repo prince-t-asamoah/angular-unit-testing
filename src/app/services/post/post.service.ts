b75e748d6736dd4d19c7541b86c7c9f08930b82b
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 09:07:34 +0000

    implement get posts service

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Post } from '../../model/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+
+  getPosts() {
+    return this._httpClient.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+  }
 }
