92c24bba8422eb022bc71251d1f2390cc20dbcf3
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 10:17:29 +0000

    add case should create post component

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,12 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { PostComponent } from './post.component';
 
 describe('PostsComponent', () => {
-  let component: PostComponent;
+  let component: PostComponent | null;
+  let fixture: ComponentFixture<PostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PostComponent],
+      providers: [provideHttpClient(withFetch())],
     }).compileComponents();
+    fixture = TestBed.createComponent(PostComponent);
+    component = null;
+  });
+
+  it('should create post component', () => {
+    component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 });
