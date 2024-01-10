63b67a515c0fb1ea030e372929465bc04c21a341
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 07:52:13 +0000

    create logger service mock for test cases

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,28 +1,30 @@
-import { LoggerService } from '../logger/logger.service';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
   //using xit and pending in test case body disable test case
 
-  const loggerService = new LoggerService();
-
   it('should add two numbers', () => {
     let expectedResult: number;
-    const calculator = new CalculatorService(loggerService);
+    const mockLoggerService = jasmine.createSpyObj('LoggerSevice', ['log']);
+    const calculator = new CalculatorService(mockLoggerService);
 
     const result = calculator.add(2, 2);
     expectedResult = 4;
 
     expect(result).toBe(expectedResult);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
     let expectedResult: number;
-    const calculator = new CalculatorService(loggerService);
+    const mockLoggerService = jasmine.createSpyObj('LoggerSevice', ['log']);
+
+    const calculator = new CalculatorService(mockLoggerService);
 
     const result = calculator.subtract(2, 2);
     expectedResult = 0;
 
     expect(result).toBe(expectedResult);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 });
