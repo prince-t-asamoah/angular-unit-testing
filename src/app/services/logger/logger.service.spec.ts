e2a7743c2839fc34416582e1d5fc5acd7b4e9c80
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 08:36:34 +0000

    add second test case

--- src/app/services/logger/logger.service.spec.ts
+++ src/app/services/logger/logger.service.spec.ts
@@ -6,4 +6,10 @@ describe('LoggerService', () => {
     let messagesCount = loggerService.messages.length;
     expect(messagesCount).toBe(0);
   });
+
+  it('should add message when log is called', () => {
+    const loggerService = new LoggerService();
+    loggerService.log('message');
+    expect(loggerService.messages.length).toBe(1);
+  });
 });
