6f2f7edcb620f596ec036ad5cfe3c945b192a4e5
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 08:08:25 +0000

    setup mocks, services and variables on each test run

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -2,12 +2,17 @@ import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
   //using xit and pending in test case body disable test case
+  let mockLoggerService: any;
+  let calculator: CalculatorService;
+  let expectedResult: number;
+
+  beforeEach(() => {
+    mockLoggerService = jasmine.createSpyObj('LoggerSevice', ['log']);
+    calculator = new CalculatorService(mockLoggerService);
+    expectedResult = 0;
+  })
 
   it('should add two numbers', () => {
-    let expectedResult: number;
-    const mockLoggerService = jasmine.createSpyObj('LoggerSevice', ['log']);
-    const calculator = new CalculatorService(mockLoggerService);
-
     const result = calculator.add(2, 2);
     expectedResult = 4;
 
@@ -16,11 +21,6 @@ describe('CalculatorService', () => {
   });
 
   it('should subtract two numbers', () => {
-    let expectedResult: number;
-    const mockLoggerService = jasmine.createSpyObj('LoggerSevice', ['log']);
-
-    const calculator = new CalculatorService(mockLoggerService);
-
     const result = calculator.subtract(2, 2);
     expectedResult = 0;
 
