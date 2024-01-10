8cef2bdfa25fe37b897406d1756654283565ecbd
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 08:57:13 +0000

    create property for post data

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../model/post';
 
 @Component({
   selector: 'app-post',
   standalone: true,
   imports: [],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+  styleUrl: './post.component.css',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  posts: Post[] = [];
 
+  ngOnInit(): void {}
 }
