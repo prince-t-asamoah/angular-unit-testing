7adadf10ed959b856dca6fe20d577ed1be0cdb09
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 12:01:04 +0000

    update service to use posts as observables

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../../model/post';
 import PostData from '../../mock/posts.json';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  constructor() {}
+  private posts = new BehaviorSubject<Post[]>(PostData);
+  public postsSubscription$ = this.posts.asObservable();
 
   getPosts(): Post[] {
-    return PostData;
+    return this.posts.getValue();
   }
 
-  deletePost(post: Post): Post[] {
-    const filteredPostData = PostData.filter(
+  deletePost(post: Post): void {
+    const postsData = this.posts.getValue();
+    const filteredPosts = postsData.filter(
       (postItem) => postItem.id !== post.id
     );
-    return filteredPostData;
+    this.posts.next(filteredPosts);
   }
 }
