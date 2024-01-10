c1eca5cb5c6d69bd36f7b16a7b146ea114efc7bb
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 11:28:52 +0000

    set route for post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PostComponent } from './components/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'post',
+        component: PostComponent
+    }
+];
