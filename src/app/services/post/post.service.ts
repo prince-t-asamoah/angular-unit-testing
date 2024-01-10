41f808f7245edfc8a50674df12c28f2000b5954d
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2024-01-10 09:09:14 +0000

    implement delete post service

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -3,13 +3,20 @@ import { Injectable } from '@angular/core';
 import { Post } from '../../model/post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
-
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private _httpClient: HttpClient) {}
 
   getPosts() {
-    return this._httpClient.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+    return this._httpClient.get<Post[]>(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+  }
+
+  deletePost(post: Post) {
+    return this._httpClient.delete(
+      `https://jsonplaceholder.typicode.com/post/${post.id}`
+    );
   }
 }
